ce4f6c1b7dac8d5f2f3af5069c6c232e1fa73930
zdtiio <zdtiio@163.com>
2025-03-12 13:41:22 +0800

    Update main_sync.sh
    修改子任务shell脚本存放位置

--- sh/main_sync.sh
+++ sh/main_sync.sh
@@ -7,28 +7,28 @@ SYNC_LOG="/home/remote_backup/log/main_sync_log_$(date +%Y-%m-%d_%H-%M-%S).log"
 echo "$(date): Starting sync tasks..." >> $SYNC_LOG
 
 # 运行第一个同步任务
-bash /home/remote_backup/sh/task1_remote_backup.sh
+bash /home/remote_backup/sh/tasks/task1_remote_backup.sh
 if [ $? -ne 0 ]; then
     echo "$(date): task1 failed." >> $SYNC_LOG
     exit 1
 fi
 
 # 运行第二个同步任务
-bash /home/remote_backup/sh/task2_remote_backup.sh
+bash /home/remote_backup/sh/tasks/task2_remote_backup.sh
 if [ $? -ne 0 ]; then
     echo "$(date): task2 failed." >> $SYNC_LOG
     exit 1
 fi
 
 # 运行第三个同步任务
-bash /home/remote_backup/sh/task3_remote_backup.sh
+bash /home/remote_backup/sh/tasks/task3_remote_backup.sh
 if [ $? -ne 0 ]; then
     echo "$(date): task3 failed." >> $SYNC_LOG
     exit 1
 fi
 
 # 运行第四个同步任务
-bash /home/remote_backup/sh/task4_remote_backup.sh
+bash /home/remote_backup/sh/tasks/task4_remote_backup.sh
 if [ $? -ne 0 ]; then
     echo "$(date): task4 failed." >> $SYNC_LOG
     exit 1
